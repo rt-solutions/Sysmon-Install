1dbc2ecadc31044ccb74e5acd8f240524d789f1f
rt-solutions <82833403+rt-solutions@users.noreply.github.com>
2021-12-27 19:50:28 -0600

    Update Sysmon_install.ps1

--- Sysmon_install.ps1
+++ Sysmon_install.ps1
@@ -1,4 +1,4 @@
-﻿### Make temp folder
+### Make temp folder
 $path = "C:\temp\"
 If(!(test-path $path))
 {
@@ -8,8 +8,8 @@ New-Item -ItemType Directory -Force -Path $path
 $serviceName = 'sysmon64'
 If (-not(Get-Service $serviceName)){
 ### DL Sysmon
-invoke-webrequest "https://ewr1.vultrobjects.com/hosted/Sysmon64.exe" -OutFile C:\temp\Sysmon64.exe
-wget "https://ewr1.vultrobjects.com/hosted/sysmonconfig.xml" -OutFile C:\windows\sysmonconfig.xml 
+invoke-webrequest "https://ewr1.vultrobjects.com/download/Sysmon64.exe" -OutFile C:\temp\Sysmon64.exe
+wget "https://ewr1.vultrobjects.com/download/sysmonconfig.xml" -OutFile C:\windows\sysmonconfig.xml 
 # Install Sysmon
 C:\temp\Sysmon64.exe /accepteula -i c:\windows\sysmonconfig.xml
  }
