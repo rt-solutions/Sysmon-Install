f244c3f5e581fee626b71b782f6b48d150eeb6d5
rt-solutions <82833403+rt-solutions@users.noreply.github.com>
2021-12-27 19:34:09 -0600

    Add files via upload

---
+++ Sysmon_install.ps1
@@ -0,0 +1,15 @@
+﻿### Make temp folder
+$path = "C:\temp\"
+If(!(test-path $path))
+{
+New-Item -ItemType Directory -Force -Path $path
+}
+### Check for service and if not install
+$serviceName = 'sysmon64'
+If (-not(Get-Service $serviceName)){
+### DL Sysmon
+invoke-webrequest "https://ewr1.vultrobjects.com/hosted/Sysmon64.exe" -OutFile C:\temp\Sysmon64.exe
+wget "https://ewr1.vultrobjects.com/hosted/sysmonconfig.xml" -OutFile C:\windows\sysmonconfig.xml 
+# Install Sysmon
+C:\temp\Sysmon64.exe /accepteula -i c:\windows\sysmonconfig.xml
+ }
