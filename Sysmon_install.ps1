cf69e2303a137509d3244d8f70b77a7b31597114
rt-solutions <82833403+rt-solutions@users.noreply.github.com>
2023-04-15 18:12:41 -0500

    Update Sysmon_install.ps1

--- Sysmon_install.ps1
+++ Sysmon_install.ps1
@@ -1,15 +1,25 @@
+# RT Solutions
+# v1 4-15-23
 ### Make temp folder
 $path = "C:\temp\"
 If(!(test-path $path))
 {
 New-Item -ItemType Directory -Force -Path $path
 }
+cd C:\temp\
 ### Check for service and if not install
 $serviceName = 'sysmon64'
 If (-not(Get-Service $serviceName)){
 ### DL Sysmon
-invoke-webrequest "https://s3.us-central-1.wasabisys.com/downloadsrts/onboarding/Sysmon64.exe" -OutFile C:\temp\Sysmon64.exe
-wget "https://s3.us-central-1.wasabisys.com/downloadsrts/onboarding/sysmonconfig.xml" -OutFile C:\windows\sysmonconfig.xml 
+invoke-webrequest "https://download.sysinternals.com/files/Sysmon.zip" -OutFile C:\temp\Sysmon.zip
+### Unzip
+Expand-Archive C:\temp\Sysmon.zip -DestinationPath C:\temp\ -force
+### DL Config File
+wget "https://raw.githubusercontent.com/SwiftOnSecurity/sysmon-config/master/sysmonconfig-export.xml" -OutFile C:\windows\sysmonconfig.xml 
 # Install Sysmon
 C:\temp\Sysmon64.exe /accepteula -i c:\windows\sysmonconfig.xml
- }else{write-host -ForegroundColor green "Sysmon Installed"}
+### Cleanup
+rm C:\temp\sysmon*
+ }
+ else {write-host "Sysmon Already Installed"}
+ 
