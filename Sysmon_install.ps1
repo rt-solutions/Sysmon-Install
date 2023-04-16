34eb0fa4db4ac67e53f59b8f3eefa16616e3289e
rt-solutions <82833403+rt-solutions@users.noreply.github.com>
2023-04-15 19:21:34 -0500

    Update Sysmon_install.ps1

--- Sysmon_install.ps1
+++ Sysmon_install.ps1
@@ -1,25 +1,41 @@
 # RT Solutions
 # v1 4-15-23
-### Make temp folder
+### 
+$serviceName = 'sysmon64'
+
 $path = "C:\temp\"
+try {
+    $Current_Version = (Get-Command C:\windows\sysmon64.exe).FileVersionInfo.ProductVersion
+} catch {
+    $Current_Version = 0
+}
 If(!(test-path $path))
 {
 New-Item -ItemType Directory -Force -Path $path
 }
 cd C:\temp\
-### Check for service and if not install
-$serviceName = 'sysmon64'
-If (-not(Get-Service $serviceName)){
 ### DL Sysmon
+rm C:\temp\sysmon*
 invoke-webrequest "https://download.sysinternals.com/files/Sysmon.zip" -OutFile C:\temp\Sysmon.zip
+
 ### Unzip
 Expand-Archive C:\temp\Sysmon.zip -DestinationPath C:\temp\ -force
+$DL_Version = (Get-Command C:\temp\sysmon64.exe).FileVersionInfo.ProductVersion
+
+if ($Current_Version -lt $DL_Version) {
+#$Current_Version = (Get-Command C:\windows\sysmon64.exe).FileVersionInfo.ProductVersion
+
+write-host "Sysmon Update needed, current version is $Current_Version, updating to $DL_Version." -ForegroundColor Green
 ### DL Config File
 wget "https://raw.githubusercontent.com/SwiftOnSecurity/sysmon-config/master/sysmonconfig-export.xml" -OutFile C:\windows\sysmonconfig.xml 
-# Install Sysmon
-C:\temp\Sysmon64.exe /accepteula -i c:\windows\sysmonconfig.xml
+### Uninstall Sysmon
+C:\windows\sysmon64.exe -u
+
+### Install Sysmon
+C:\temp\sysmon64.exe /accepteula -i c:\windows\sysmonconfig.xml
+Write-host "Up to date, version" (Get-Command C:\windows\sysmon64.exe).FileVersionInfo.ProductVersion
 ### Cleanup
 rm C:\temp\sysmon*
- }
- else {write-host "Sysmon Already Installed"}
- 
+} else {
+    Write-Output "Sysmon up to date"
+}
